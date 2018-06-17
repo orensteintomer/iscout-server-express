0ad1f9aae5da83ad7073e900395274f5fb78f072
tomerorenstein <orensteintomer@hotmail.com>
2018-06-18 01:34:14 +0300

    Add websocket of offers between scouters and players

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const config = require('./lib/config');
 const boom = require('express-boom');
@@ -22,5 +23,32 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 swagger.setUpRoutes(middlewareObj, app, swaggerDocument, useBasePath);
-app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+
+// web sockets
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+server.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+
+// user_id => socket.id
+let connectedUsers = {};
+
+io.sockets.on('connection', function (socket) {
+
+    // when the client emits 'sendoffer', this listens and executes
+    socket.on('adduser', function (userId) {
+
+        // Adds current user
+        connectedUsers[userId] = socket.id;
+    });
+
+    // when the client emits 'sendoffer', this listens and executes
+    socket.on('sendoffer', function (data) {
+
+        // Gets destination socket id
+        let destSocketId = connectedUsers[data.dest_user_id];
+
+        // we tell the client to execute 'gotoffer' with 2 parameters
+        io.to(destSocketId).emit('gotoffer', data);
+    });
+});
 
